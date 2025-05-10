c43a9d5050caf51050b337c15ae6a7150473ea99
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-05-10 23:18:32 +0300

    Added script LinesGrid

---
+++ Assets/Project/Scripts/GridCoordinate.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridCoordinate : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _line1;
+    [SerializeField]
+    private List<GameObject> _line2;
+    [SerializeField]
+    private List<GameObject> _line3;
+    [SerializeField]
+    private List<GameObject> _line4;
+    [SerializeField]
+    private List<GameObject> _line5;
+    [SerializeField]
+    private List<GameObject> _line6;
+    [SerializeField]
+    private List<GameObject> _line7;
+
+
+}

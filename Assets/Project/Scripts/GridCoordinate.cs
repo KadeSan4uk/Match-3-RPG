1cb9433b966d500426852600218f0379aaede1b7
Oleksandr <145229696+KadeSan4uk@users.noreply.github.com>
2025-05-10 23:35:51 +0300

    Added Script Grid Coordinate

--- Assets/Project/Scripts/GridCoordinate.cs
+++ Assets/Project/Scripts/GridCoordinate.cs
@@ -18,5 +18,17 @@ public class GridCoordinate : MonoBehaviour
     [SerializeField]
     private List<GameObject> _line7;
 
+    private List<List<GameObject>> _grid = new List<List<GameObject>>();
 
+
+    private void Awake()
+    {
+        _grid.Add(_line1);
+        _grid.Add(_line2);
+        _grid.Add(_line3);
+        _grid.Add(_line4);
+        _grid.Add(_line5);
+        _grid.Add(_line6);
+        _grid.Add(_line7);
+    }
 }
